77334cdfe3b2119800e45b663a6d4b39aa9a7c67
sohyun123 <carriesohyun@gmail.com>
2022-07-01 17:06:39 +0900

    update: calibrating2 scene
    SizeManager.cs updated

--- WonderScope_2022/Assets/Scripts/SizeManager.cs
+++ WonderScope_2022/Assets/Scripts/SizeManager.cs
@@ -9,21 +9,13 @@ public class SizeManager : MonoBehaviour
 {
 
     public Slider sizeSlider;
-    // public Sprite rulerImg;
-    // public Image rulerImg;
     public SpriteRenderer rulerImg;
     public TextMeshProUGUI scaleTxt;
 
     public void ChangeSize()
     {
         float scale = sizeSlider.value;
-        // rulerImg.texture.Reinitialize(scale, scale);
-        // rulerImg.texture.Apply();
-
-        // rulerImg.rectTransform.localScale = new Vector3(rulerImg.Height*scale,scale,1);
         rulerImg.transform.localScale = new Vector3(scale, scale, 1);
-
-
     }
 
     void Update()
@@ -31,13 +23,4 @@ public class SizeManager : MonoBehaviour
         scaleTxt.text = Convert.ToString(sizeSlider.value);
     }
 
-    // void Start()
-    // {
-
-    // }
-
-    // void Update()
-    // {
-
-    // }
 }
