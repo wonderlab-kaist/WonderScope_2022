4116a126ec8177f1b18a7b95ffb3c40fc1062a2a
sohyun123 <carriesohyun@gmail.com>
2022-07-18 23:41:02 +0900

    update: cave scene updated

--- WonderScope_2022/Assets/Scenes/Cave/Demo/CaveGameController.cs
+++ WonderScope_2022/Assets/Scenes/Cave/Demo/CaveGameController.cs
@@ -8,31 +8,25 @@ using UnityEngine.SceneManagement;
 
 public class CaveGameController : MonoBehaviour
 {
+    public aarcall aar;
+
     public GameObject Cam;
-    //public Transform Crystals;
+    public Transform Crystals;
+    public GameObject effect1;
+    public GameObject effect2;
+    public AudioSource crystalSound;
+    public TextMeshProUGUI scoreTxt;
+
     private GameObject Crystal;
-    //Transform[] allChildren = GetComponentsInChildren<Transform>();
+    private int score=0;
+    private float distance=70f;
 
-    //private int score;
-    //private int distance;
-    private int count = 0;
-    //private float camdist;
 
-    //public ParticleSystem twinkle;
-    public GameObject twinkle;
-    public TextMeshProUGUI score;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //score = 0;
-        //distance = 30;
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+        scoreTxt.text = "SCORE: " + Convert.ToString(score);
+
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             Touch touch = Input.GetTouch(0);
             var ray = Camera.main.ScreenPointToRay(touch.position);
@@ -42,94 +36,66 @@ public class CaveGameController : MonoBehaviour
                 var rig = hitInfo.collider.GetComponent<Rigidbody>();
                 if (rig != null)
                 {
-                    print(rig.name);
                     Crystal = GameObject.Find(rig.name);
-                    Instantiate(twinkle, Crystal.transform.position, Quaternion.identity);
+                    crystalSound.time = 1;
+                    crystalSound.Play();
+                    Instantiate(effect1, Crystal.transform.position, Quaternion.identity);
+                    Destroy(GameObject.Find("Effect_" + Crystal.name));
+                    Crystal.tag = "EffectOff";
                     Crystal.SetActive(false);
-                    count++;
+                    score++;
+                    aar.vibrate_phone();
                 }
             }
         }
 
-        if(Input.touchCount == 3) SceneManager.LoadScene(0, LoadSceneMode.Single);
+        if (Input.touchCount == 3) SceneManager.LoadScene(0, LoadSceneMode.Single);
 
-        score.text = "SCORE: " + Convert.ToString(count);
 
+        #region ForDebugging
         //if (Input.GetMouseButtonDown(0))
         //{
-        //    var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    Vector3 touchpos = Input.mousePosition;
+        //    var ray = Camera.main.ScreenPointToRay(touchpos);
         //    RaycastHit hitInfo;
         //    if (Physics.Raycast(ray, out hitInfo))
         //    {
         //        var rig = hitInfo.collider.GetComponent<Rigidbody>();
         //        if (rig != null)
         //        {
-        //            print(rig.name);
         //            Crystal = GameObject.Find(rig.name);
-        //            Instantiate(twinkle, Crystal.transform.position, Quaternion.identity);
+        //            crystalSound.time = 1;
+        //            crystalSound.Play();
+        //            Instantiate(effect1, Crystal.transform.position, Quaternion.identity);
+        //            //Instantiate(twinkle, touchpos, Quaternion.identity);
+        //            Destroy(GameObject.Find("Effect_" + Crystal.name));
+        //            Crystal.tag = "EffectOff";
         //            Crystal.SetActive(false);
+        //            score++;
         //        }
         //    }
         //}
+        #endregion
 
 
-        ////ParticleSystem pinkSystem = GetComponent<ParticleSystem>();
-        ////pinkSystem.transform.position= Crystal.transform.position;
-        ////ParticleSystem.ShapeModule _editableShape = twinkle.shape;
-        //Crystal = GameObject.Find("crystal_05 (37)");
-        //twinkle.transform.position = Crystal.transform.position;
+        foreach (Transform childCrystal in Crystals)
+        {
+            Crystal = GameObject.Find(childCrystal.name);
+            if (Vector3.Distance(Crystal.transform.position, Cam.transform.position) < distance && Crystal.activeSelf)
+            {
+                if (Crystal.tag != "EffectOn")
+                {
+                    GameObject effect = Instantiate(effect2, Crystal.transform.position, Quaternion.identity);
+                    effect.gameObject.name = "Effect_" + childCrystal.name;
 
-        ////_editableShape.position = Crystal.transform.position;
-        //if (Crystal.activeSelf)
-        //{
-        //    twinkle.Play();
-        //    Crystal.SetActive(false);
-
-        //}
-        //if (Crystals != null)
-        //{
-        //    foreach (Transform childCrystal in Crystals)
-        //    {
-        //        Crystal = GameObject.Find(childCrystal.name);
-        //        //Instantiate(twinkle, Crystal.transform.position, Quaternion.identity);
-
-        //        if (Crystal.activeSelf)
-        //        {
-        //            //_editableShape.position = Crystal.transform.position;
-
-        //            Instantiate(twinkle, Crystal.transform.position, Quaternion.identity);
-
-        //            //twinkle.transform.position = Crystal.transform.position;
-        //            //twinkle.Play();
-        //            Crystal.SetActive(false);
-
-        //        }
-        //    }
-        //}
-
-
-
-
-        // crystal의 child들 다 선회해서 active true인 것들 중에 cam이랑 거리값이 10 이내인 것?은 효과 주고 active false로 바꾸기. 그리고 score +1
-        //foreach (Transform childCrystal in Crystals)
-        //{
-        //    Crystal = GameObject.Find(childCrystal.name);
-        //    camdist = Mathf.Sqrt(Mathf.Pow((Crystal.transform.position.x - Cam.transform.position.x),2)+ Mathf.Pow((Crystal.transform.position.z - Cam.transform.position.z), 2));
-
-        //    //if ((Vector3.Distance(Crystal.transform.position, Cam.transform.position) < distance) && Crystal.activeSelf)
-        //    if ((camdist < distance) && Crystal.activeSelf)
-        //        {
-        //        //_editableshape.position = crystal.transform.position;
-
-        //        Instantiate(twinkle, Crystal.transform.position, Quaternion.identity);
-        //        //twinkle.transform.position = crystal.transform.position;
-        //        //twinkle.play();
-        //        Crystal.SetActive(false);
-
-        //    }
-        //    //crystal = gameobject.find(childcrystal.name);
-        //    //print(crystal.name);
-        //    ////print(childcrystal.);
-        //}
+                    Crystal.tag = "EffectOn";
+                }
+            }
+            else if (Vector3.Distance(Crystal.transform.position, Cam.transform.position) >= distance && Crystal.activeSelf)
+            {
+                Destroy(GameObject.Find("Effect_" + childCrystal.name));
+                Crystal.tag = "EffectOff";
+            }
+        }
     }
 }
