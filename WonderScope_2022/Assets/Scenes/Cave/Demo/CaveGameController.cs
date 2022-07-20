1526c065f84547f990edf1696aaa1a207adb033f
sohyun123 <carriesohyun@gmail.com>
2022-07-20 14:03:13 +0900

    update: cave scene updated (crystal location notifier)

--- WonderScope_2022/Assets/Scenes/Cave/Demo/CaveGameController.cs
+++ WonderScope_2022/Assets/Scenes/Cave/Demo/CaveGameController.cs
@@ -16,15 +16,30 @@ public class CaveGameController : MonoBehaviour
     public GameObject effect2;
     public AudioSource crystalSound;
     public TextMeshProUGUI scoreTxt;
+    public GameObject up;
+    public GameObject down;
+    public GameObject left;
+    public GameObject right;
 
     private GameObject Crystal;
     private int score=0;
     private float distance=70f;
+    private float cryDist;
+    private List<Vector3> closeCrystals = new List<Vector3>();
 
+    void Start()
+    {
+        up.SetActive(false);
+        down.SetActive(false);
+        left.SetActive(false);
+        right.SetActive(false);
+        closeCrystals.Add(Vector3.zero);
+
+    }
 
     void Update()
     {
-        scoreTxt.text = "SCORE: " + Convert.ToString(score);
+        scoreTxt.text = Convert.ToString(score);
 
         if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
@@ -67,7 +82,6 @@ public class CaveGameController : MonoBehaviour
         //            crystalSound.time = 1;
         //            crystalSound.Play();
         //            Instantiate(effect1, Crystal.transform.position, Quaternion.identity);
-        //            //Instantiate(twinkle, touchpos, Quaternion.identity);
         //            Destroy(GameObject.Find("Effect_" + Crystal.name));
         //            Crystal.tag = "EffectOff";
         //            Crystal.SetActive(false);
@@ -81,7 +95,9 @@ public class CaveGameController : MonoBehaviour
         foreach (Transform childCrystal in Crystals)
         {
             Crystal = GameObject.Find(childCrystal.name);
-            if (Vector3.Distance(Crystal.transform.position, Cam.transform.position) < distance && Crystal.activeSelf)
+
+            cryDist = Vector3.Distance(Crystal.transform.position, Cam.transform.position);
+            if (cryDist < distance && Crystal.activeSelf)
             {
                 if (Crystal.tag != "EffectOn")
                 {
@@ -91,11 +107,82 @@ public class CaveGameController : MonoBehaviour
                     Crystal.tag = "EffectOn";
                 }
             }
-            else if (Vector3.Distance(Crystal.transform.position, Cam.transform.position) >= distance && Crystal.activeSelf)
+            else if (cryDist >= distance && Crystal.activeSelf)
             {
                 Destroy(GameObject.Find("Effect_" + childCrystal.name));
                 Crystal.tag = "EffectOff";
             }
+
+            if (Crystal.activeSelf && cryDist > (distance + 10f))
+            {
+                if (closeCrystals[0] == Vector3.zero)
+                {
+                    closeCrystals[0] = Crystal.transform.position;
+                    closeCrystals.Add(Crystal.transform.position);
+                    closeCrystals.Add(Crystal.transform.position);
+                }
+                else if (cryDist < Vector3.Distance(closeCrystals[0], Cam.transform.position))
+                {
+                    closeCrystals[2] = closeCrystals[1];
+                    closeCrystals[1] = closeCrystals[0];
+                    closeCrystals[0] = Crystal.transform.position;
+                }
+                else if ((Vector3.Distance(closeCrystals[0], Cam.transform.position) <= cryDist) && (Vector3.Distance(closeCrystals[1], Cam.transform.position) > cryDist))
+                {
+                    closeCrystals[2] = closeCrystals[1];
+                    closeCrystals[1] = Crystal.transform.position;
+                }
+                else if ((Vector3.Distance(closeCrystals[1], Cam.transform.position) <= cryDist) && (Vector3.Distance(closeCrystals[2], Cam.transform.position) > cryDist))
+                {
+                    closeCrystals[2] = Crystal.transform.position;
+                }
+            }
         }
+
+
+        // 방향 구하기
+        foreach (Vector3 crystal in closeCrystals)
+        {
+            //Vector3 v = Cam.transform.forward-crystal;
+            //Vector3 v = Cam.transform.forward*10- crystal;
+
+            //Vector3 v = Cam.transform.position - crystal;
+            //Vector3 v = crystal - Cam.transform.position;
+
+            //var degree = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+            //float degree = Vector3.Angle(Cam.transform.forward, crystal);
+
+
+            float degree = Vector3.SignedAngle(transform.up, crystal - Cam.transform.position, -transform.forward);
+            if (degree >= -45 && degree < 45)
+            {
+                right.SetActive(true);
+                up.SetActive(false);
+                left.SetActive(false);
+                down.SetActive(false);
+            }
+            else if (degree >= 45 && degree < 135)
+            {
+                right.SetActive(false);
+                up.SetActive(true);
+                left.SetActive(false);
+                down.SetActive(false);
+            }
+            else if ((degree >= 135 && degree <= 180) || (degree >= -180 && degree < -135))
+            {
+                right.SetActive(false);
+                up.SetActive(false);
+                left.SetActive(true);
+                down.SetActive(false);
+            }
+            else if (degree >= -135 && degree < -45)
+            {
+                right.SetActive(false);
+                up.SetActive(false);
+                left.SetActive(false);
+                down.SetActive(true);
+            }
+        }
+        
     }
 }
