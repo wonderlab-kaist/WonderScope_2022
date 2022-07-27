e6000e43e440d0591564148409747bb59af68894
hyeonbeom <ihb0523@gmail.com>
2022-07-27 12:38:52 +0900

    0727_moon_scene_update

--- WonderScope_2022/Assets/Scenes/Moon/Scripts/Destroy.cs
+++ WonderScope_2022/Assets/Scenes/Moon/Scripts/Destroy.cs
@@ -7,7 +7,7 @@ public class Destroy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 5);
+        Destroy(gameObject, 2f);
     }
 
 }
