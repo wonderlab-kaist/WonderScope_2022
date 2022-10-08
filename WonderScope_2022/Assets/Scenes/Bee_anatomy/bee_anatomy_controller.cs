ef72e978877da2a374255e9aeb3b7b468d7b8563
sw981127 <57656632+sw981127@users.noreply.github.com>
2022-10-08 15:17:13 +0900

    소화계/순환계/신경계 visualize

--- WonderScope_2022/Assets/Scenes/Bee_anatomy/bee_anatomy_controller.cs
+++ WonderScope_2022/Assets/Scenes/Bee_anatomy/bee_anatomy_controller.cs
@@ -5,9 +5,15 @@ using TMPro;
 
 public class bee_anatomy_controller : MonoBehaviour
 {
-    public GameObject bee_anatomy;
+    public GameObject bee_anatomy_default;
+    public GameObject bee_anatomy_resp;
+    public GameObject bee_anatomy_digest;
+    public GameObject bee_anatomy_nerve;
+
     public TextMeshProUGUI anatomy_sys_txt;
     public TextMeshProUGUI explanation;
+
+    private float viz_btn;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +26,38 @@ public class bee_anatomy_controller : MonoBehaviour
     {
         
 
+    }
+
+    public void set_visualize(float viz)
+    {
+        viz_btn = viz;
+        if (viz == 0)
+        {
+            bee_anatomy_default.SetActive(true);
+            bee_anatomy_resp.SetActive(true);
+            bee_anatomy_digest.SetActive(true);
+            bee_anatomy_nerve.SetActive(true);
+        }
+        else if (viz == 1)
+        {
+            bee_anatomy_default.SetActive(true);
+            bee_anatomy_resp.SetActive(true);
+            bee_anatomy_digest.SetActive(false);
+            bee_anatomy_nerve.SetActive(false);
+        }
+        else if (viz ==2)
+        {
+            bee_anatomy_default.SetActive(true);
+            bee_anatomy_resp.SetActive(false);
+            bee_anatomy_digest.SetActive(true);
+            bee_anatomy_nerve.SetActive(false);
+        }
+        else if (viz == 3)
+        {
+            bee_anatomy_default.SetActive(true);
+            bee_anatomy_resp.SetActive(false);
+            bee_anatomy_digest.SetActive(false);
+            bee_anatomy_nerve.SetActive(true);
+        }
     }
 }
